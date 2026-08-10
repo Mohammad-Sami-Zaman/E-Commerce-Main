23dfbfac603d5bed57a736265ef9fa5ef9d251ba
Mohammad Sami Zaman <mohammad.sami.zaman@g.bracu.ac.bd>
2026-08-10 20:14:15 +0600

    Update urls.py

--- Sami_007_product/urls.py
+++ Sami_007_product/urls.py
@@ -6,4 +6,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('productmanager.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
